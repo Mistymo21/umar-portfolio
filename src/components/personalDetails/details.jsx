e41f9b6f75cbb95ff28fc7c5871523751c0b7ce3
Mistymo21 <yusufmm90999@gmail.com>
2025-05-25 16:11:15 +0100

    Fixtures

--- src/components/personalDetails/details.jsx
+++ src/components/personalDetails/details.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import styles from "./details.module.css";
 import Image from "next/image";
-import Bee from "../../../public/bee.jpeg";
+import Personal from "../../../public/personal.jpg";
 import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 const details = () => {
@@ -11,22 +11,22 @@ const details = () => {
       {" "}
       <div className={styles.personal}>
         <h1>Yusuf Muhammed</h1>
-        <Image src={Bee} alt="" className={styles.img} />
+        <Image src={Personal} alt="" className={styles.img} />
         <div className={styles.desc}>
           <p>Based in Nigeria</p>
-          <span> Frontend Web Developer</span>
+          <span> FullStack Website Developer</span>
 
           <div className={styles.icons}>
-            <Link href="#">
+            <Link href="https://www.facebook.com/share/1Bht3yLme1/?mibextid=qi2Omg">
               <FaFacebook />
             </Link>
-            <Link href="#">
+            <Link href="https://x.com/omoibileponpon?t=h67wlCOeVsnAtqEDvvL6tQ&s=09">
               <FaXTwitter />
             </Link>
-            <Link href="#">
+            <Link href="https://www.linkedin.com/in/yusuf-muhammed-06029b321?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
               <FaLinkedin />
             </Link>
-            <Link href="#">
+            <Link href="https://github.com/Mistymo21">
               <FaGithub />
             </Link>
           </div>
